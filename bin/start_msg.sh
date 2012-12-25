30420a3d0b0aee2e9c14ff650aa44c5cbe15ea52
Ian Yang <me@iany.me>
2012-12-26 01:52:30 +0800

    only start daemons

--- bin/start_msg.sh
+++ bin/start_msg.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
-if ! tmux has-session -t mutt; then
-  env TMUX= tmux start-server \; set-option -g base-index 1 \; new-session -d -s mutt -n mutt
-  tmux new-window -t mutt:2 -n daemons
+if ! tmux has-session -t msg; then
+  env TMUX= tmux start-server \; set-option -g base-index 1 \; new-session -d -s msg -n daemons
 
-  tmux send-keys -t mutt:1 mutt C-m
-  tmux send-keys -t mutt:2 "cd ~/codebase/daemons; foreman start" C-m
+  tmux send-keys -t msg:1 "cd ~/codebase/daemons; foreman start" C-m
 
-  set-window-option -t mutt:2 monitor-activity off
-  set-window-option -t mutt:2 monitor-silence 600
+  set-window-option -t msg:1 monitor-activity off
+  set-window-option -t msg:1 monitor-silence 600
 fi
 
 if [ "$1" = "attach" ]; then
   if [ -z $TMUX ]; then
-    tmux -u attach-session -t mutt
+    tmux -u attach-session -t msg
   else
-    tmux -u switch-client -t mutt
+    tmux -u switch-client -t msg
   fi
 fi
