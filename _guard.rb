42c387f995dd30eb5eb2f775a946bfe4abacc73d
Ian Yang <me@iany.me>
2013-01-31 11:41:46 +0800

    update

--- _guard.rb
+++ _guard.rb
@@ -1,22 +1,26 @@
-require 'guard/notifiers/emacs'
-require 'guard/notifiers/tmux'
+require 'guard/notifier'
 
-if ::Guard::Notifier::Emacs.available?(true)
-  notification :emacs, {
+notifiers_options = {
+  :emacs => {
     :fontcolor => "#acbc90",
     :default => "#1e2320",
     :success => "#013009",
     :failed => "#310602",
     :pending => "#534626"
-  }
-end
-
-if ::Guard::Notifier::Tmux.available?(true)
-  notification :tmux, {
+  },
+  :tmux => {
     :success                => 'colour64',
     :failed                 => 'colour124',
     :pending                => 'colour136',
     :default                => 'colour239',
     :display_message        => false,
+  }
+}
+
+::Guard::Notifier::NOTIFIERS.each do |group|
+  group.map { |n| n.first }.find { |notifier|
+    ::Guard::Notifier.add_notification(notifier,
+                                       notifiers_options[notifier] || {},
+                                       true)
   }
 end
