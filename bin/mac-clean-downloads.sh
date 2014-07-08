cba82335114ecc21bd247f269bcfa41e08403489
Ian Yang <me@iany.me>
2014-07-08 14:40:20 +0800

    use modified time to clean downloads directory

--- bin/mac-clean-downloads.sh
+++ bin/mac-clean-downloads.sh
@@ -2,7 +2,7 @@
 export PATH="/usr/local/bin:$PATH"
 
 cd ~/Downloads/
-find . Thunder -depth 1 ! -name '.*' ! -name '$*' ! -name 'desktop.ini' ! -path './Old' ! -path './RTX' ! -path './Snapshots' ! -path './Thunder' \( \( -Btime -4d -exec tag -a New '{}' \; \) -o -true \) \( \( \( -Btime +4d -o -Btime 4d \) -exec tag -r New '{}' \; \) -o -true \) \( \( -Btime +15d -exec mv '{}' ~/Downloads/Old/ \; \) -o -true \) > /dev/null
+find . Thunder -depth 1 ! -name '.*' ! -name '$*' ! -name 'desktop.ini' ! -path './Old' ! -path './RTX' ! -path './Snapshots' ! -path './Thunder' \( \( -mtime -4d -exec tag -a New '{}' \; \) -o -true \) \( \( \( -mtime +4d -o -mtime 4d \) -exec tag -r New '{}' \; \) -o -true \) \( \( -mtime +15d -exec mv '{}' ~/Downloads/Old/ \; \) -o -true \) > /dev/null
 
-find Snapshots RTX -depth 1 ! -name '.*' ! -name '$*' ! -name 'desktop.ini' -Btime +7d -print0 | xargs -0 ~/.rm-trash/rm.rb -rf --no-color > /dev/null
+find Snapshots RTX -depth 1 ! -name '.*' ! -name '$*' ! -name 'desktop.ini' -mtime +7d -print0 | xargs -0 ~/.rm-trash/rm.rb -rf --no-color > /dev/null
 
