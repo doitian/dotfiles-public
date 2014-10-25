ce35671816a29842f7b698a0a72aeb40f8611ae6
Ian Yang <me@iany.me>
2014-10-26 04:09:44 +0800

    remove mac clean downloads script

--- bin/mac-clean-downloads.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export PATH="/usr/local/bin:$PATH"
-
-cd ~/Downloads/
-find . Thunder -depth 1 ! -name '.*' ! -name '$*' ! -name 'desktop.ini' ! -path './Old' ! -path './RTX' ! -path './Snapshots' ! -path './Thunder' ! -path './keyiDownload' \( \( -mtime -4d -exec tag -a new '{}' \; \) -o -true \) \( \( \( -mtime +4d -o -mtime 4d \) -exec tag -r new '{}' \; \) -o -true \) \( \( -mtime +15d -exec mv '{}' ~/Downloads/Old/ \; \) -o -true \) > /dev/null
-
-find Snapshots RTX -depth 1 ! -name '.*' ! -name '$*' ! -name 'desktop.ini' -mtime +7d -print0 | xargs -0 ~/.rm-trash/rm.rb -rf --no-color > /dev/null
