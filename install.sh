5325690c245d1952f65fa7af831ee5d4cabaa429
Ian Yang <me@iany.me>
2015-03-03 19:14:01 +0800

    Check fresh before clone

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-git clone https://github.com/freshshell/fresh ~/.fresh/source/freshshell/fresh
+set -e
+
+if ! [ -d ~/.fresh/source/freshshell/fresh ]; then
+  git clone https://github.com/freshshell/fresh ~/.fresh/source/freshshell/fresh
+fi
+
 ln -snf ~/.dotfiles/freshrc ~/.freshrc
+
 ~/.fresh/source/freshshell/fresh/bin/fresh # run fresh
