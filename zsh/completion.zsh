9904594fe20a4717658b7db84cc1e08672396b60
Ian Yang <me@iany.me>
2015-03-08 09:37:43 +0800

    update zsh config

--- zsh/completion.zsh
+++
@@ -1,5 +0,0 @@
-function _tmux-up-dwim() {
-  compadd $(tmux list-session &> /dev/null| awk -F: '{print $1}'; find -L ~/.tmux-up -name '*.conf' | sed -e 's:.*/::' -e 's:.conf$::')
-}
-
-compdef _tmux-up-dwim tmux-up-dwim
