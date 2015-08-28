7a813116e63fd6d09a99c9b763cc2db8fee61dd7
ian <me@iany.me>
2015-08-28 14:37:23 +0800

    update

--- zsh/docker.zsh
+++ zsh/docker.zsh
@@ -1,20 +1,19 @@
 function tmux-docker() {
-  unset DYLD_LIBRARY_PATH
-  unset LD_LIBRARY_PATH
+  local machine=${1:-dev}
+
   unset DOCKER_HOST
   unset DOCKER_CERT_PATH
   unset DOCKER_TLS_VERIFY
-  tmux set-environment -u DYLD_LIBRARY_PATH
-  tmux set-environment -u LD_LIBRARY_PATH
+  unset DOCKER_MACHINE_NAME
   tmux set-environment -u DOCKER_HOST
   tmux set-environment -u DOCKER_CERT_PATH
   tmux set-environment -u DOCKER_TLS_VERIFY
-  mkdir -p ~/.boot2docker
-  /usr/local/bin/boot2docker init
-  /usr/local/bin/boot2docker up
-  eval $(/usr/local/bin/boot2docker shellinit)
+  tmux set-environment -u DOCKER_MACHINE_NAME
+  eval "$(docker-machine env $machine)"
+
   tmux set-environment DOCKER_HOST "$DOCKER_HOST"
   tmux set-environment DOCKER_CERT_PATH "$DOCKER_CERT_PATH"
   tmux set-environment DOCKER_TLS_VERIFY "$DOCKER_TLS_VERIFY"
+  tmux set-environment DOCKER_MACHINE_NAME "$DOCKER_MACHINE_NAME"
   docker version
 }
