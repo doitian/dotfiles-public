9175ea8f5fe8209aee894ea255d206de510fa0b9
ian <me@iany.me>
2016-12-05 18:34:20 +0800

    refactor tt

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -20,3 +20,29 @@ function _tmux-fzf-pane() {
   _arguments '-k[Kill selected panes]' '-p[Show preview window]' '-a[Show all panes in all sessions]' '-s[Show all panes in active session]' "::session:{_describe 'panes' panes}"
 }
 compdef _tmux-fzf-pane tmux-fzf-pane
+
+function _tt() {
+  local expl state
+  local -a panes
+  panes=( ${(f)"$(command tmux 2> /dev/null list-panes -F '#S:#W.#P')"} )
+  _arguments \
+    '-l[Send keys literally, do not recoganize keycode name]' \
+    '-c[Commit, a.k.a., send a C-m at the end]' \
+    "-t[Send to target, or specify where to create new pane]:panes:->panes" \
+    - '(Creation Options)' \
+    '-s[Send keys to new session]' \
+    '-S[Send keys to new session with specified name]:session name' \
+    '-n[Send keys to new window]' \
+    '-N[Send keys to new window with specified name]:window name' \
+    '-h[Send keys to new pane split horizontally]' \
+    '-v[Send keys to new pane split vertically]' && return 0
+  
+  case "$state" in
+    panes)
+      _multi_parts : panes
+  esac
+
+  return 1
+}
+
+compdef _tt tt
