de427fdfb6652c6960e6ed7b021c799f1c48d435
ian <me@iany.me>
2017-01-20 10:11:08 +0800

    update

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -71,6 +71,16 @@ fi
 
 alias di='dirs -v | head -n 10'
 
+alias -- -1='cd -1'
+alias -- -2='cd -2'
+alias -- -3='cd -3'
+alias -- -4='cd -4'
+alias -- -5='cd -5'
+alias -- -6='cd -6'
+alias -- -7='cd -7'
+alias -- -8='cd -8'
+alias -- -9='cd -9'
+
 ##################################################
 # System
 alias psg="ps auxw | grep -i"
