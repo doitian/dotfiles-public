eb92b0923f0d807203f083d944bfd37ee0dbf104
ian <me@iany.me>
2017-09-26 19:13:05 +0800

    update

--- zsh/ruby.zsh
+++ zsh/ruby.zsh
@@ -1,34 +1,37 @@
 export RBENV_SHELL=zsh
-compctl -K _rbenv rbenv
 
-_rbenv() {
-  local words completions
-  read -cA words
+if command -v rbenv > /dev/null; then
+  compctl -K _rbenv rbenv
 
-  if [ "${#words}" -eq 2 ]; then
-    completions="$(rbenv commands)"
-  else
-    completions="$(rbenv completions ${words[2,-2]})"
-  fi
+  _rbenv() {
+    local words completions
+    read -cA words
 
-  reply=("${(ps:\n:)completions}")
-}
+    if [ "${#words}" -eq 2 ]; then
+      completions="$(rbenv commands)"
+    else
+      completions="$(rbenv completions ${words[2,-2]})"
+    fi
 
-#rbenv rehash 2>/dev/null
-rbenv() {
-  typeset command
-  command="$1"
-  if [ "$#" -gt 0 ]; then
-    shift
-  fi
+    reply=("${(ps:\n:)completions}")
+  }
 
-  case "$command" in
-  rehash|shell)
-    eval `rbenv "sh-$command" "$@"`;;
-  *)
-    command rbenv "$command" "$@";;
-  esac
-}
+  #rbenv rehash 2>/dev/null
+  rbenv() {
+    typeset command
+    command="$1"
+    if [ "$#" -gt 0 ]; then
+      shift
+    fi
+
+    case "$command" in
+    rehash|shell)
+      eval `rbenv "sh-$command" "$@"`;;
+    *)
+      command rbenv "$command" "$@";;
+    esac
+  }
+fi
 
 alias hbundle='bundle install --path vendor/bundle'
 sbundle() {
