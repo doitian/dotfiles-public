750765ec5e55bbf11152b11a01fab5d850910f92
ian <me@iany.me>
2017-10-12 07:31:59 +0800

    gi -s uses fzf to complete gitignore

--- zsh/gitignore_fast.zsh
+++ zsh/gitignore_fast.zsh
@@ -1,14 +1,18 @@
 function gi() {
   local curcontext=:complete
   local _gitignore_list gitignore_type
-  if [ -z "$1" -o "$1" = "list" ]; then
+  if [ -z "$1" -o "$1" = "-l" -o "$1" = "-s" ]; then
     if ! _retrieve_cache gitignore_list; then
       _gitignore_list=( $(curl -sL https://www.gitignore.io/api/list | tr "," "\n") )
       _store_cache gitignore_list _gitignore_list
     fi
-    echo "${_gitignore_list[*]}" | tr " " "\n"
+    if [ "$1" = "-s" ]; then
+      echo "${_gitignore_list[*]}" | tr " " "\n" | fzf -m | xargs -I % -L1 curl -sL "https://www.gitignore.io/api/%"
+    else
+      echo "${_gitignore_list[*]}" | tr " " "\n"
+    fi
   else
-    curl -sL https://www.gitignore.io/api/$@
+    curl -sL "https://www.gitignore.io/api/$1"
   fi
 }
 
