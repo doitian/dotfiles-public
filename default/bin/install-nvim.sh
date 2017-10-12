a7d8aea5483dafe525aed995532653260bc0e8e7
ian <me@iany.me>
2017-10-12 12:15:37 +0800

    Revert "Revert "delete install-nvim.sh""
    This reverts commit 6e3a097d8ff81e86664cdc07dd41b3d0f05d677e.

--- default/bin/install-nvim.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -u
-
-target_dir="$HOME/Applications"
-rm -rf "$target_dir/nvim-osx64"
-curl -L -o - https://github.com/neovim/neovim/releases/download/nightly/nvim-macos.tar.gz | tar -xzvf - -C "$target_dir"
