d4914a7eb4d6384205fa91eb7e2c0d9adbaeb6f9
ian <me@iany.me>
2019-05-17 17:34:30 +0800

    only enable timingapp when Timing is found

--- zsh/timingapp.zsh
+++ zsh/timingapp.zsh
@@ -1,9 +1,11 @@
-if [ -z "$HOSTNAME" ]; then
-  HOSTNAME=$(hostname -f)
+if [ -d /Applications/Setapp/Timing.app ] || [ -d /Applications/Timing.app ]; then
+  if [ -z "$HOSTNAME" ]; then
+    HOSTNAME=$(hostname -f)
+  fi
+
+  timingapp_precmd() {
+    echo -ne "\033]0;${USER}@${HOSTNAME%%.*}:${PWD/#$HOME/~}\007"
+  }
+
+  add-zsh-hook precmd timingapp_precmd
 fi
-
-timingapp_precmd() {
-  echo -ne "\033]0;${USER}@${HOSTNAME%%.*}:${PWD/#$HOME/~}\007"
-}
-
-add-zsh-hook precmd timingapp_precmd
