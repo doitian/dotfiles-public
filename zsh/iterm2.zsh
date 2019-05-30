52394ed5e3da4ca87068fc6935774e3c8e4cfb7b
ian <me@iany.me>
2019-05-30 12:50:57 +0800

    cleanup

--- zsh/iterm2.zsh
+++ zsh/iterm2.zsh
@@ -1,3 +1,3 @@
-if [ -f "$HOME/.iterm2_shell_integration.zsh" ]; then
+if [ "${TERM_PROGRAM:-}" = "iTerm.app" ] && [ -f "$HOME/.iterm2_shell_integration.zsh" ]; then
   source "$HOME/.iterm2_shell_integration.zsh"
 fi
