6f1450f420e936eba5fbefb22e3547990fe09719
ian <me@iany.me>
2023-04-13 12:46:54 +0800

    allow running upgrade-venv-python.sh against a folder

--- default/bin/upgrade-venv-python.sh
+++ default/bin/upgrade-venv-python.sh
@@ -6,6 +6,8 @@ set -u
 
 FROM_VERSION="$1"
 TO_VERSION="$2"
+shift
+shift
 
 upgrade() {
   local venv="$1"
@@ -16,10 +18,16 @@ upgrade() {
   ln -snf "python-$FROM_VERSION" "$TO_VERSION_VENV"
 }
 
-for venv in "$HOME"/codebase/*/.direnv/python-"$FROM_VERSION"; do
-  upgrade "$venv"
-done
+if [ "$#" = 0 ]; then
+  for venv in "$HOME"/codebase/*/.direnv/python-"$FROM_VERSION"; do
+    upgrade "$venv"
+  done
 
-for venv in "$HOME"/.dirsenv/*/python-"$FROM_VERSION"; do
-  upgrade "$venv"
-done
+  for venv in "$HOME"/.dirsenv/*/python-"$FROM_VERSION"; do
+    upgrade "$venv"
+  done
+else
+  for venv; do
+    upgrade "$venv"
+  done
+fi
