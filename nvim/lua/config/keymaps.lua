2cd36d88ef899d6ed30b2eb67bad5551b67d5e9f
ian <me@iany.me>
2023-07-27 16:49:24 +0800

    cleanup layzvim

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,5 +1,3 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-
-vim.keymap.set("n", "<Leader>n", "<cmd>noh<cr>", { desc = "Clear hlsearch" })
--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -22,7 +22,7 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.lang.json" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
-    { import = "plugins" },
+    -- { import = "plugins" },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
