e898a8cc231902b44731428d69d5390c05a961e0
ian <me@iany.me>
2023-07-29 13:56:18 +0800

    vim: enable project

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -24,6 +24,8 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "lazyvim.plugins.extras.test.core" },
+    { import = "lazyvim.plugins.extras.util.project" },
+    -- { import = "lazyvim.plugins.extras.ui.edgy" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "plugins" },
