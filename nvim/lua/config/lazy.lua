f6ec0e60a54040200b064a9df23556842fb1f983
ian <me@iany.me>
2023-07-29 13:34:46 +0800

    vim: import prettier formatting

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -19,9 +19,10 @@ require("lazy").setup({
     },
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.coding.copilot" },
+    { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.lang.go" },
-    { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "lazyvim.plugins.extras.lang.python" },
+    { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "lazyvim.plugins.extras.test.core" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
