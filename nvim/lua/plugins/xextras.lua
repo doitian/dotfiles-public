1059794142f19ae2b73ddd80571a21f24e72fd5a
ian <me@iany.me>
2023-08-07 13:31:49 +0800

    neovim: remove project

--- nvim/lua/plugins/xextras.lua
+++ nvim/lua/plugins/xextras.lua
@@ -4,16 +4,8 @@ return {
   { import = "lazyvim.plugins.extras.coding.copilot" },
   -- { import = "lazyvim.plugins.extras.dap.core" },
   -- { import = "lazyvim.plugins.extras.test.core" },
+  -- IDE like window layout
   -- { import = "lazyvim.plugins.extras.ui.edgy" },
   -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
   { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
-  { import = "lazyvim.plugins.extras.util.project" },
-
-  {
-    "ahmedkhalf/project.nvim",
-    optional = true,
-    opts = {
-      patterns = { ".git", "!=.oh-my-zsh", "!=.asdf" },
-    },
-  },
 }
