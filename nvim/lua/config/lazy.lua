a3368d5da4086e9ef5a6aebcd7f6375c2ff395df
ian <me@iany.me>
2023-08-08 17:36:42 +0800

    lazy: use --filter=tree:0 to clone lazy.nvim

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -2,8 +2,7 @@ local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   -- bootstrap lazy.nvim
   -- stylua: ignore
-  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable",
-    lazypath })
+  vim.fn.system({ "git", "clone", "--filter=tree:0", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
 end
 vim.opt.rtp:prepend(lazypath)
 vim.opt.rtp:append(vim.env.HOME .. ".dotfiles/repos/private/nvim")
@@ -25,11 +24,8 @@ require("lazy").setup({
       optional = true,
       opts = {
         highlight_overrides = {
-          latte = function(latte)
-            return {
-              NeoTreeCursorLine = { bg = latte.base },
-            }
-          end,
+          -- stylua: ignore
+          latte = function(latte) return { NeoTreeCursorLine = { bg = latte.base } } end,
         },
       },
     },
