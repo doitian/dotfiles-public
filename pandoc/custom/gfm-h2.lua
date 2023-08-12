1fde574057ed168c4418640d0af241fb8ccad9a4
ian <me@iany.me>
2023-08-12 15:22:53 +0800

    pandoc: remove logging

--- pandoc/custom/gfm-h2.lua
+++ pandoc/custom/gfm-h2.lua
@@ -1,18 +1,16 @@
-local logging = require 'logging'
-
 function Writer(doc, opts)
   local filter = {
-    Header = function (h)
+    Header = function(h)
       h.level = h.level + 1
       return h
-    end
+    end,
   }
   doc = doc:walk(filter)
   table.insert(doc.blocks, 1, pandoc.Header(1, doc.meta.title))
-  return pandoc.write(doc, 'gfm', opts)
+  return pandoc.write(doc, "gfm", opts)
 end
 
 function Template()
   local template = pandoc.template
-  return template.default('gfm')
+  return template.default("gfm")
 end
