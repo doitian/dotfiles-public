5228784e93e81bda2626bcc47ec6a3e513565c03
ian <me@iany.me>
2023-09-11 17:35:39 +0800

    github-changelog: EXTRA_PRS

--- default/bin/github-changelog.py
+++ default/bin/github-changelog.py
@@ -47,9 +47,6 @@ else:
     since = _str(subprocess.check_output(
         ['git', 'describe', '--tags', tag_rev]).strip())
 
-logs = _str(subprocess.check_output(
-    ['git', 'log', '--reverse', '--merges', '--pretty=tformat:%s', '{}...HEAD'.format(since)]))
-
 PR_NUMBER_RE = re.compile(r'^Merge pull request #(\d+) from')
 BORS_PR_NUMBER_RE = re.compile(r'^Merge #(\d+.*)')
 PR_TITLE_RE = re.compile(
@@ -77,9 +74,13 @@ SCOPE_TITLE = {
     'refactor': 'Improvements',
 }
 
-for line in logs.splitlines():
-    pr_numbers = []
+pr_numbers = []
+if 'EXTRA_PRS' in os.environ:
+    pr_numbers = os.environ['EXTRA_PRS'].split(',')
 
+logs = _str(subprocess.check_output(
+    ['git', 'log', '--reverse', '--merges', '--pretty=tformat:%s', '{}...HEAD'.format(since)]))
+for line in logs.splitlines():
     pr_number_match = PR_NUMBER_RE.match(line)
     if pr_number_match:
         pr_numbers.append(pr_number_match.group(1))
@@ -88,88 +89,88 @@ for line in logs.splitlines():
         for pr_number in pr_number_match.group(1).split(" #"):
             pr_numbers.append(pr_number)
 
-    for pr_number in pr_numbers:
-        cache_file = ".git/changes/{}.json".format(pr_number)
-        if os.path.exists(cache_file):
-            print("read pr #" + pr_number, file=sys.stderr)
-            with open(cache_file) as fd:
-                pr = json.load(fd)
-        else:
-            print("get pr #" + pr_number, file=sys.stderr)
-            api_endpoint = 'https://api.github.com/repos/{}/pulls/{}'.format(
-                repo, pr_number)
-            pr_resp = get(api_endpoint)
-            if pr_resp.status == 404:
-                print("PR {} not found".format(pr_number), file=sys.stderr)
-                continue
-
-            pr = json.loads(pr_resp.read().decode())
-            if 'message' in pr:
-                print(pr['message'], file=sys.stderr)
-                sys.exit(1)
-
-            with open(cache_file, 'w') as fd:
-                json.dump(pr, fd)
-
-        scope, module, message = PR_TITLE_RE.match(pr['title']).groups()
-        if not scope:
-            if message.lower().startswith("fix ") or message.lower().startswith("fixes "):
-                scope = 'fix'
-            else:
-                scope = 'misc'
-        scope = SCOPE_MAPPING.get(scope, scope)
-        if not scope:
+for pr_number in pr_numbers:
+    cache_file = ".git/changes/{}.json".format(pr_number)
+    if os.path.exists(cache_file):
+        print("read pr #" + pr_number, file=sys.stderr)
+        with open(cache_file) as fd:
+            pr = json.load(fd)
+    else:
+        print("get pr #" + pr_number, file=sys.stderr)
+        api_endpoint = 'https://api.github.com/repos/{}/pulls/{}'.format(
+            repo, pr_number)
+        pr_resp = get(api_endpoint)
+        if pr_resp.status == 404:
+            print("PR {} not found".format(pr_number), file=sys.stderr)
             continue
 
-        user = pr['user']['login']
-        message = message.strip()
-        message = message[0].upper() + message[1:]
-        if module:
-            title = '* #{0} **{1}:** {2} (@{3})'.format(pr_number,
-                                                        module, message, user)
+        pr = json.loads(pr_resp.read().decode())
+        if 'message' in pr:
+            print(pr['message'], file=sys.stderr)
+            sys.exit(1)
+
+        with open(cache_file, 'w') as fd:
+            json.dump(pr, fd)
+
+    scope, module, message = PR_TITLE_RE.match(pr['title']).groups()
+    if not scope:
+        if message.lower().startswith("fix ") or message.lower().startswith("fixes "):
+            scope = 'fix'
         else:
-            title = '* #{0}: {1} (@{2})'.format(pr_number, message, user)
+            scope = 'misc'
+    scope = SCOPE_MAPPING.get(scope, scope)
+    if not scope:
+        continue
 
-        change = Change(scope, module, title, [])
-        Change = namedtuple('Change', ['scope', 'module', 'title', 'text'])
+    user = pr['user']['login']
+    message = message.strip()
+    message = message[0].upper() + message[1:]
+    if module:
+        title = '* #{0} **{1}:** {2} (@{3})'.format(pr_number,
+                                                    module, message, user)
+    else:
+        title = '* #{0}: {1} (@{2})'.format(pr_number, message, user)
 
-        body = pr['body'] or ""
-        if '```release-note' in body:
-            # ```release-note
-            # None: Exclude this PR from the release note.
-            # Title Only: Include only the PR title in the release note.
-            # Note: Add a note under the PR title in the release note.
-            # ```
-            body = body.split('```release-note')[1].split('```')[0].strip()
-            if body.startswith('None: '):
-                continue
-            elif body.startswith('Title Only: '):
+    change = Change(scope, module, title, [])
+    Change = namedtuple('Change', ['scope', 'module', 'title', 'text'])
+
+    body = pr['body'] or ""
+    if '```release-note' in body:
+        # ```release-note
+        # None: Exclude this PR from the release note.
+        # Title Only: Include only the PR title in the release note.
+        # Note: Add a note under the PR title in the release note.
+        # ```
+        body = body.split('```release-note')[1].split('```')[0].strip()
+        if body.startswith('None: '):
+            continue
+        elif body.startswith('Title Only: '):
+            body = ''
+        elif body.startswith('Note: '):
+            body = body[6:]
+            if body.strip() == 'Add a note under the PR title in the release note.':
                 body = ''
-            elif body.startswith('Note: '):
-                body = body[6:]
-                if body.strip() == 'Add a note under the PR title in the release note.':
-                    body = ''
 
-        labels = [label['name'] for label in pr['labels']]
-        is_breaking = "breaking change" in labels or any(
-            label.startswith('b:') for label in labels)
-        if is_breaking:
-            breaking_banner = ", ".join(
-                label for label in labels if label.startswith('b:'))
-            if breaking_banner != "" or "breaking change" not in body.lower():
-                if breaking_banner == "":
-                    breaking_banner = "This is a breaking change"
-                else:
-                    breaking_banner = "This is a breaking change: " + breaking_banner
-            if body == "":
-                body = breaking_banner
-            elif breaking_banner != "":
-                body = breaking_banner + "\n\n" + body
+    labels = [label['name'] for label in pr['labels']]
+    is_breaking = "breaking change" in labels or any(
+        label.startswith('b:') for label in labels)
+    if is_breaking:
+        breaking_banner = ", ".join(
+            label for label in labels if label.startswith('b:'))
+        if breaking_banner != "" or "breaking change" not in body.lower():
+            if breaking_banner == "":
+                breaking_banner = "This is a breaking change"
+            else:
+                breaking_banner = "This is a breaking change: " + breaking_banner
+        if body == "":
+            body = breaking_banner
+        elif breaking_banner != "":
+            body = breaking_banner + "\n\n" + body
 
-        if scope not in changes:
-            changes[scope] = []
+    if scope not in changes:
+        changes[scope] = []
 
-        changes[scope].append(Change(scope, module, title, body))
+    changes[scope].append(Change(scope, module, title, body))
 
 if os.path.exists(".git/changes/extra.json"):
     with open(".git/changes/extra.json") as fin:
