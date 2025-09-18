ef3845d954af970a14c4ac56cd0a4d4b38d525e0
ian <me@iany.me>
2025-09-18 22:23:34 +0800

    remove temporary fixing to LazyVim

--- nvim/lua/plugins/overrides.lua
+++ nvim/lua/plugins/overrides.lua
@@ -142,13 +142,6 @@ return {
         }
       )
     end,
-    -- https://github.com/LazyVim/LazyVim/pull/6354#issuecomment-3202799735
-    init = function()
-      local bufline = require("catppuccin.groups.integrations.bufferline")
-      function bufline.get()
-        return bufline.get_theme()
-      end
-    end,
   },
 
   -- editor {{{1
