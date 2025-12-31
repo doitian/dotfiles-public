9d2640dc47b2f07bca14ac554cb3c752a30785dd
ian <me@iany.me>
2026-01-01 02:02:59 +0800

    make mise preset:rust platform compatible

--- mise/tasks/preset/rust.ps1
+++ mise/tasks/preset/rust.ps1
@@ -4,8 +4,8 @@
 mise tasks add pre-commit:cargo-fmt -- cargo fmt --check
 
 if (Get-Command -ErrorAction Ignore cargo-nextest) {
-  mise config set tasks.test.run 'cargo nextest run --no-fail-fast ${usage_filters}'
+  mise config set tasks.test.run 'cargo nextest run --no-fail-fast{% for f in usage.filters | default(value=[]) %} {{ f }}{% endfor %}'
 } else {
-  mise config set tasks.test.run 'cargo test --no-fail-fast -- ${usage_filters}'
+  mise config set tasks.test.run 'cargo test --no-fail-fast --{% for f in usage.filters | default(value=[]) %} {{ f }}{% endfor %}'
 }
 mise config set tasks.test.usage 'arg "[filters]" var=#true'
