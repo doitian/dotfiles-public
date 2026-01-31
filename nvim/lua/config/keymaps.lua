f92497aa9a3c6cc69af76e5b78191ae60a4c0975
ian <me@iany.me>
2026-01-31 21:57:40 +0800

    lazy: open yazi

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -74,6 +74,9 @@ map("n", "<leader>ws", "<C-W>s", { desc = "Split window below", remap = true })
 map("n", "<leader>wv", "<C-W>v", { desc = "Split window right", remap = true })
 
 map("n", "<Leader>fj", "<Cmd>drop `jrnl -p`<CR>", { desc = "Edit journal" })
+map("n", "<Leader>fy", function()
+  Snacks.terminal.toggle("yazi")
+end, { desc = "Yazi" })
 
 -- coding {{{1
 map("n", "<Leader>cw", "<Cmd>ru macros/buffer/whitespace.vim<CR>", { desc = "Fix whitespace issues" })
