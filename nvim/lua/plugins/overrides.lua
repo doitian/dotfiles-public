670efe99d4c7cf64a1c99418a5683cebd2446500
ian <me@iany.me>
2026-08-06 05:37:15 +0800

    add temporary flash.nvim workaround for nvim-0.13 compatibility
    This adds a temporary configuration override for flash.nvim to support
    Neovim 0.13 by manually defining missing FFI symbols and implementing
    required helper functions. The override should be removed once upstream
    PR #492 is merged.

--- nvim/lua/plugins/overrides.lua
+++ nvim/lua/plugins/overrides.lua
@@ -185,6 +185,64 @@ return {
   {
     "folke/flash.nvim",
     optional = true,
+    -- Remove when https://github.com/folke/flash.nvim/pull/492 merges
+    config = function(_, opts)
+      if vim.fn.has("nvim-0.13") == 1 then
+        local Hacks = require("flash.hacks")
+        local ffi = require("ffi")
+        if not pcall(function()
+          return ffi.C.search_match_lines
+        end) then
+          pcall(
+            ffi.cdef,
+            [[
+              typedef struct {
+                bool hl_match;
+                int32_t match_lines;
+                int match_endcol;
+                int32_t first_line;
+                int32_t last_line;
+                bool no_smartcase;
+                int cmdlen;
+                bool no_hlsearch;
+              } SearchState;
+              SearchState Search;
+            ]]
+          )
+          if pcall(function()
+            return ffi.C.Search.match_lines
+          end) then
+            local C = ffi.C
+            local Pos = require("flash.search.pos")
+            local incsearch_state = {}
+
+            function Hacks.get_end_pos(from)
+              local ret = Pos({
+                from[1] + C.Search.match_lines,
+                math.max(0, C.Search.match_endcol - 1),
+              })
+              local line = vim.api.nvim_buf_get_lines(0, ret[1] - 1, ret[1], false)[1]
+              local char_idx = vim.fn.charidx(line, ret[2])
+              ret[2] = vim.fn.byteidx(line, char_idx)
+              return ret
+            end
+
+            function Hacks.save_incsearch_state()
+              incsearch_state = {
+                match_endcol = C.Search.match_endcol,
+                match_lines = C.Search.match_lines,
+              }
+            end
+
+            function Hacks.restore_incsearch_state()
+              C.Search.match_endcol = incsearch_state.match_endcol
+              C.Search.match_lines = incsearch_state.match_lines
+            end
+          end
+        end
+      end
+      require("flash").setup(opts)
+    end,
     keys = {
       -- Use `z` because `s` is used by surround
       {
